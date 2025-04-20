29e083e8a7c6ed879dd5cdb4b149f257df2b2e05
ivmen <ivan.mendez.pacheco@gmail.com>
2025-04-20 19:02:47 +0200

    last

--- my-portfolio/data/projects.js
+++ my-portfolio/data/projects.js
@@ -1,10 +1,8 @@
-
-
 export const projects = [
     {
         title: "Arquitectura de la información",
         description: "Pagina web de informacion sobre el grupo de heavy metal Scorpions",
-        image: "assets/scorpions.png",
+        image: "/assets/scorpions.png", // Ruta corregida
         tech: ["HTML5"],
         github: "https://github.com/ivmen/html5_arquitectura_informacion",
         link: "Arquitectura de la informacion"
@@ -12,7 +10,7 @@ export const projects = [
     {
         title: "Landing Page",
         description: "Pagina web dedicada a crear un landing page como practica para The Power",
-        image:  "assets//landing-page.png",
+        image: "/assets/landing-page.png", // Ruta corregida
         tech: ["HTML5", "CSS3"],
         github: "https://github.com/ivmen/entrega_flex",
         link: "Landing Page"
@@ -20,7 +18,7 @@ export const projects = [
     {
         title: "Proyecto 1",
         description: "Pagina web dedicada a crear un landing page parecida a la pagina web de MediaMarkt",
-        image: "assets/media-markt.png",
+        image: "/assets/media-markt.png", // Ruta corregida
         tech: ["HTML5", "CSS3"],
         github: "https://github.com/ivmen/Proyecto-1",
         link: "Proyecto 1"
@@ -28,9 +26,9 @@ export const projects = [
     {
         title: "Practica DOM",
         description: "Pagina web dedicada a crear una pagina de venta de articulo parecida a la pagina web de MediaMarkt",
-        image: "assets/DOM.png",
+        image: "/assets/DOM.png", // Ruta corregida
         tech: ["HTML5", "CSS3", "JavaScript"],
         github: "https://github.com/ivmen/Practica-DOM",
         link: "Practica DOM"
     },
-]
+];
